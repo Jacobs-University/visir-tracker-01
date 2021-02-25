4f016cf18e021dbaebb8bfd7aebd109b5373354d
Yonatan Mamo <yonatan.mamo@faro.com>
2021-02-25 06:13:49 +0100

    problem1.1

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,33 @@
 
 int main() {
 	VideoCapture camera;
-	if (!camera.open(0)) {
+	if (!camera.open(1)) {
 		printf("Can't find a camera\n");
 		return 1;
 	};
 	
 	// Main loop
 	Mat img;
+	double tframe = 0;
+	double frame = 0;
+
 	for(;;) {
+		double t = cv::getTickCount();
+
 		camera >> img;
 		imshow("Camera", img);
-		int key = waitKey(5);
+		int key = waitKey(5);	
 		if (key == 27 || key == 'q') break;
+
+		tframe += (cv::getTickCount() - t) / cv::getTickFrequency();
+		frame++; 
+		if (tframe >= 2.0) {
+			std::cout << "FPS: " <<  frame / tframe << std::endl;
+			tframe = 0;
+			frame = 0;
+		}
+		
+
 	}
 	camera.release();
 	return 0;
