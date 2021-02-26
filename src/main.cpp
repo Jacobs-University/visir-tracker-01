2da24d7ceeeb549e005c6a8019a1be80306d9cb5
Yonatan Mamo <yonatan.mamo@faro.com>
2021-02-26 03:10:56 +0100

    face detector

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,19 @@ void detectAndDisplay(Mat frame);
 int main() {
 	VideoCapture camera;
 
-	if (!camera.open(1)) {
+	if (!camera.open(0)) {
 		printf("Can't find a camera\n");
 		return 1;
 	};
 
 	const std::string face_cascade_name = "C:/Users/Yonathan/Downloads/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml";
-	const std::string eyes_cascade_name = "C:/Users/Yonathan/Downloads/opencv/sources/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
-
-	//load cascades
+	//load cascade
 	if (!face_cascade.load(face_cascade_name))
 	{
 		std::cout << "--(!)Error loading face cascade\n";
 		return -1;
 	};
-	if (!eyes_cascade.load(eyes_cascade_name))
-	{
-		std::cout << "--(!)Error loading eyes cascade\n";
-		return -1;
-	};
-	
+
 	// Main loop
 	Mat img;
 	double elapsed = 0;
@@ -35,7 +28,6 @@ int main() {
 
 	for(;;) {
 		double t = cv::getTickCount();
-
 		camera >> img;
 		//imshow("Camera", img);
 		detectAndDisplay(img);
@@ -44,17 +36,15 @@ int main() {
 
 		elapsed += (cv::getTickCount() - t) / cv::getTickFrequency();
 		frame++;
-		
+
 		if (elapsed >= 2.0) {
 			//average
 			std::cout << "FPS: " <<  frame / elapsed << std::endl;
 			elapsed = 0;
-			frame = 0;
-			
+			frame = 0;	
 			//instantaneous 
 			//std::cout << "FPS: " << 1 / (double)((cv::getTickCount() - t) / cv::getTickFrequency()) << std::endl;
 		}
-
 	}
 	camera.release();
 	return 0;
@@ -64,6 +54,7 @@ int main() {
 
 void detectAndDisplay(Mat frame)
 {
+	//https://docs.opencv.org/4.2.0/db/d28/tutorial_cascade_classifier.html
 	Mat frame_gray;
 	frame_gray.convertTo(frame_gray, CV_64F);
 	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
@@ -76,15 +67,6 @@ void detectAndDisplay(Mat frame)
 		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
 		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
 		Mat faceROI = frame_gray(faces[i]);
-		//-- In each face, detect eyes
-		std::vector<Rect> eyes;
-		eyes_cascade.detectMultiScale(faceROI, eyes);
-		for (size_t j = 0; j < eyes.size(); j++)
-		{
-			Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
-			double radius = cvRound(((double)eyes[j].width + (double)eyes[j].height) * 0.25);
-			circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
-		}
 	}
 	imshow("Capture - Face detection", frame);
 }
