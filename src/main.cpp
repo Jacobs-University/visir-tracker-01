ce6c58dd335c1ea746c95723d1b239abbac70e63
alxmeen <82112218+alxmeen@users.noreply.github.com>
2021-09-22 12:39:27 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
-#include "types.h"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <numeric>
+
+using namespace cv;
+
+
+template <typename T>
+T average(std::vector<T> const& v) {
+	if (v.empty()) {
+		return 0;
+	}
+	return std::accumulate(v.begin(), v.end(), 0.0) / v.size();
+}
 
 int main() {
 	VideoCapture camera;
@@ -6,13 +20,35 @@ int main() {
 		printf("Can't find a camera\n");
 		return 1;
 	};
-	
+
 	// Main loop
 	Mat img;
-	for(;;) {
+	std::vector<long> fpsCont;
+
+	long frameCounter = 0;
+
+	std::time_t timeBegin = std::time(0);
+	int tick = 0;
+	
+	for (;;) {
 		camera >> img;
 		imshow("Camera", img);
+		
 		int key = waitKey(5);
+	
+		frameCounter++;
+		fpsCont.push_back(frameCounter);
+		
+		std::time_t timeNow = std::time(0) - timeBegin;
+		if (timeNow - tick >= 1)
+		{
+			tick+=2;
+			std::cout << "Average Frames per second: " << average(fpsCont) << std::endl;
+			frameCounter = 0;
+			fpsCont.clear();
+		}
+
+
 		if (key == 27 || key == 'q') break;
 	}
 	camera.release();
