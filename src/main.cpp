89abc35ddc6bd6a3e6bc38687bdeb45c0154dea3
kleenZZi <calin.clichici@gmail.com>
2021-09-26 18:57:25 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ CascadeClassifier eyes_cascade;
 
 int main(int argc, const char** argv) {
 
+	// Set up parser
 	CommandLineParser parser(argc, argv,
 		"{help h||}"
 		"{face_cascade|data/haarcascades/haarcascade_frontalface_alt.xml|Path to face cascade.}"
@@ -21,10 +22,11 @@ int main(int argc, const char** argv) {
 		"You can use Haar or LBP features.\n\n");
 	parser.printMessage();
 
+	// Set up cascades
 	String face_cascade_name = samples::findFile(parser.get<String>("face_cascade"));
 	String eyes_cascade_name = samples::findFile(parser.get<String>("eyes_cascade"));
 
-	//-- 1. Load the cascades
+	// Load the cascades
 	if (!face_cascade.load(face_cascade_name))
 	{
 		std::cout << "--(!)Error loading face cascade\n";
@@ -36,6 +38,7 @@ int main(int argc, const char** argv) {
 		return -1;
 	};
 
+	// Check if camera is available
 	VideoCapture camera;
 	if (!camera.open(0)) {
 		printf("Can't find a camera\n");
@@ -60,30 +63,36 @@ int main(int argc, const char** argv) {
 	for(;;) 
 	{
 		// Start clock for FPS estimation
+		// Happens before image processing begins
 		start = clock();
 
+		// Get image from camera
 		camera >> img;
 
 		// Detect face and draw recognition
 		img = detect(img);
 
+		// Show the image with facial regonition features
 		imshow("Camera", img);
+
+		// End clock for FPS estimation
+		// Happens after image processing ends
+		end = clock();
+
+		// Detect quit key
 		int key = waitKey(5);
 		if (key == 27 || key == 'q') break;
 
-		// End clock for FPS estimation
-		end = clock();
-
-		// Calculate number of seconds elapsed during time capture
+		// Calculate number of seconds elapsed during image processing
 		double seconds = (double(end) - double(start)) / double(CLOCKS_PER_SEC);
 
 		// Calculate average FPS
 		fps = double(num_frames) / double(seconds);
 
-		// Take current timer status
+		// Take current clock value
 		timerEnd = clock();
 
-		// Recalculate number of seconds
+		// Recalculate number of seconds to estimate elapsed time so far
 		seconds = (double(timerEnd) - double(timerStart)) / double(CLOCKS_PER_SEC);
 
 		// If elapsed time greater than 2 seconds, print FPS and reset timer 
@@ -98,15 +107,19 @@ int main(int argc, const char** argv) {
 	return 0;
 }
 
+// Function to detect face and eyes
 Mat detect(Mat img)
 {
+	// Initialize blank mat for gray image
 	Mat img_gray;
 	cvtColor(img, img_gray, COLOR_BGR2GRAY);
 	equalizeHist(img_gray, img_gray);
 
+	// Initialize vector for faces
 	std::vector<Rect> faces;
 	face_cascade.detectMultiScale(img_gray, faces);
 
+	// Process and recognize face and eyes
 	for (size_t i = 0; i < faces.size(); i++)
 	{
 		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
@@ -114,7 +127,7 @@ Mat detect(Mat img)
 
 		Mat faceROI = img_gray(faces[i]);
 
-		//-- In each face, detect eyes
+		// In each face, detect eyes
 		std::vector<Rect> eyes;
 		eyes_cascade.detectMultiScale(faceROI, eyes);
 
@@ -126,5 +139,6 @@ Mat detect(Mat img)
 		}
 	}
 
+	// Return image with detection circles
 	return img;
 }
