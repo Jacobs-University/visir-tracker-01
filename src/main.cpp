dcc7e62aeec561b56bd7e5d18cfe0c4c743d9f23
Khurshid Ernazarov <61235723+techforces@users.noreply.github.com>
2021-09-27 23:25:00 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-#include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
-#include <iostream>
-#include <chrono>
-#include <thread>
-#include <atomic>
 
 using namespace cv;
 using namespace std;
